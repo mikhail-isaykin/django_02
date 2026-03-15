919bea14534ffeaa50d47a8ee4c69df41de40ad3
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-03-15 22:27:52 +0300

    add: top-3 customers by total order sum with ranking

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 from django.db.models import F, Window, Sum, Count, ExpressionWrapper, DecimalField
 from django.db.models.functions import Rank, DenseRank
-from traning_app.models import Product
+from traning_app.models import Product, Customer, Order
 
 
 Product.objects.annotate(
@@ -17,3 +17,13 @@ Product.objects.annotate(
         order_by=F('rating').desc()
     )
 ).values('name', 'rating', 'rank')[:5]
+
+
+Customer.objects.annotate(orders_count=Count("orders")).filter(orders_count__gt=0).annotate(total_sum=ExpressionWrapper(
+    Sum("orders__total"),
+    output_field=DecimalField()),
+    rank=Window(
+        expression=Rank(),
+        order_by=F("total_sum").desc()
+    )
+).values("name", "total_sum", "rank")[:3]
