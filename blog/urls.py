2645e67eea983cd5bd752a33d35c62e482ba6178
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-03-16 22:20:33 +0300

    feat: add routes and views

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('greet/<str:name>/', views.greet_name, name='greet_name'),
+    path('posts/<int:post_id>', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("Добро пожаловать в мой блог!")
+    return HttpResponse('Добро пожаловать в мой блог!')
+
+
+def greet_name(request, name):
+    return HttpResponse(f'Привет, {name}')
+
+
+def post_detail(request, post_id):
+    return HttpResponse(f'Вы просматриваете пост с ID: {post_id}')
