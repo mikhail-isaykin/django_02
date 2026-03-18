3fff2325d8419c8d53998c1fc1d555cdd7d1206d
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-03-18 23:55:36 +0300

    feat: implement product_list_by_category view (in progress)

--- traning_app/views.py
+++ traning_app/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from .models import Product, Category
 
 
-def product_list_by_category(request, category_name):
-    pass
+def product_list_by_category(request, category_name=None):
+    if Category.objects.filter(name=category_name).exists()
+    data = Product.objects.filter(category__name=category_name)
+    if data:
+        .....
