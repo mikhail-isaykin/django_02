c9e5d1242ff3e78e1bdbb460887f480dff65a0f7
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-03-22 22:54:56 +0300

    feat: add product list view filtered by category name

--- traning_app/views.py
+++ traning_app/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from .models import Product, Category
+from django.template import Template, Context
 
 
 def product_list_by_category(request, category_name=None):
-    if Category.objects.filter(name=category_name).exists()
-    data = Product.objects.filter(category__name=category_name)
-    if data:
-        .....
+    if Category.objects.filter(name=category_name).exists():
+        data = Product.objects.filter(category__name=category_name)
+        if data:
+            html = '''
+                <h1>Продукты в категории {{ category_name }}</h1>
+                <ul>
+                {% for product in data %}
+                    <li>{{ product.name }} (Цена: {{ product.price }}, Рейтинг: {{ product.rating }})</li>
+                {% endfor %}
+                </ul>
+            '''
+            template = Template(html)
+            context = Context({'category_name': category_name, 'data': data})
+            return HttpResponse(template.render(context))
+        return HttpResponse('Продуктов нет')
+    return HttpResponse('Такой категории нет')
