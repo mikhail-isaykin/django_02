5334dcc2da80caff79e9447f4483043fc42d09eb
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-06 22:55:23 +0300

    chore: clean up previous project files
    Remove all files from previous task-solving project to start fresh

--- blog/api_v1_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.api_v1_data, name='v1_data')
-]
--- traning_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TraningAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'traning_app'
