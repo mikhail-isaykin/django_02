80ca44c2294f3bec94608fc6e0822073ae82117f
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-09 23:40:18 +0300

    feat: add HomePageView with category filtering and templates directory

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Category, Product
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = ''
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.filter(is_active=True)
+        selected_category = self.request.get.GET('category')
+        products = Product.objects.filter(category_name=selected_category)
+        context['categories'] = categories
+        context['selected_category'] = selected_category
+        context['products'] = products
+        return context
