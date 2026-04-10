b2bbf4c14ab047e314aecdd721a04300cdcb1d16
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-11 02:14:05 +0300

    feat: add CartPurchaseView with balance check and atomic transaction

---
+++ cart/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CartDetailView
+
+
+urlpatterns = [
+    path('', CartDetailView.as_view(), name='detail'),
+]
--- cart/views.py
+++ cart/views.py
@@ -5,8 +5,12 @@ from .models import Product, Cart, CartItem
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
+from django.views.generic import View, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from accounts.models import BankAccount
+from django.db import transaction
+
 
 
 @login_required
@@ -37,3 +41,25 @@ class CartDetailView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['cart'] = self.cart
         return context
+
+
+class CartPurchaseView(LoginRequiredMixin, View):
+    def post(self, request):
+        user = request.user
+        cart = Cart.objects.filter(user=user).first()
+        bank_account = BankAccount.objects.filter(user=user).first()
+        if not cart or not bank_account:
+            messages.error(request, 'Отсутствует корзина или банковский счет')
+            return redirect('cart_detail')
+        total = sum(cart_item.product.price * cart_item.quantity for cart_item in cart.items.all())
+        if bank_account.balance < total:
+            messages.error(request, 'Недостаточно средств')
+            return redirect('cart_detail')
+        with transaction.atomic():
+            bank_account = BankAccount.objects.select_for_update().get(user=user)
+            bank_account.balance -= total
+            bank_account.save()
+            cart.items.all().delete()
+        messages.success(request, 'Покупка успешно оформлена')
+        return redirect('home')
+            
