49f9b6133a98b358abe23d21eb4d664f847dd2fb
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-11 14:35:37 +0300

    feat(accounts): add bank account view with login required

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import BankAccount
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+@login_required
+def bank_account_view(request):
+    bank_account = get_object_or_404(BankAccount, user=request.user)
+    return render(
+        request,
+        'accounts/bank_account.html',
+        {'bank_account': bank_account}
+    )
