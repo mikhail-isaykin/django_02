3bd413a5b0dc94541f8fbf2840601488ae8d9c53
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-20 01:18:08 +0300

    feat: normalize username to lowercase before saving via pre_save signal

--- products/signals.py
+++ products/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Profile
@@ -9,3 +9,7 @@ User = get_user_model()
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(pre_save, sender=User)
+def normalize_username(sender, instance, **kwargs):
+    instance.username = instance.username.lower()
