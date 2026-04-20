96cf400359ed94d7ba9c77f7ae44415e52a0af48
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-20 22:57:52 +0300

    feat: add post_save signal to notify on Article create and update

--- products/signals.py
+++ products/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Product, Photo, Article
+
 User = get_user_model()
 import os
 from django.utils.text import slugify
@@ -11,13 +12,23 @@ from django.utils.text import slugify
 def normalize_username(sender, instance, **kwargs):
     instance.username = instance.username.lower()
 
+
 @receiver(post_delete, sender=Photo)
 def delete_photo_file(sender, instance, **kwargs):
     if os.path.isfile(instance.image.path):
         os.remove(instance.image.path)
         print('Файл удален')
 
+
 @receiver(pre_save, sender=Article)
 def auto_generate_article_slug(sender, instance, **kwargs):
     if not instance.slug:
         instance.slug = slugify(instance.title)
+
+
+@receiver(post_save, sender=Article)
+def notify_order_status(sender, instance, created, **kwargs):
+    if created:
+        print('Создан новый объект')
+    else:
+        print('Объект обновлен')
