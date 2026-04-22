9b8ddb96488455fe922cb8134b1cfc095a6a083d
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-22 23:55:48 +0300

    feat: add divide function with zero division error logging

--- logger.py
+++ logger.py
@@ -16,6 +16,19 @@ logger.addHandler(console)
 
 logger.info('Программа запущена')
 
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        logger.error('Деление на ноль', exc_info=True)
+
+result1 = divide(10, 2)
+print(f"Результат деления 10 на 2: {result1}")
+                  
+
+result2 = divide(10, 0)
+print(f"Результат деления 10 на 0: {result2}")
+
 if __name__ == "__main__":
     logger.debug("debug сообщение")
     logger.info("info сообщение")
