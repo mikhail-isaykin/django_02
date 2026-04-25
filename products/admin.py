37abd7d90b83d6feae9af20a7f25c63f6531a34a
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-04-25 15:34:55 +0300

    feat: add ProfileAdmin with list_display and fieldsets

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Profile
 
 
 @admin.register(Article)
@@ -8,4 +8,11 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ['slug']
     list_filter = ['author']
     search_fields = ['title']
-    
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'website', 'location']
+    fieldsets = [
+        ('Личная информация', {'fields': ['user', 'bio']}),
+        ('Дополнительно', {'fields': ['website', 'location']}),
+    ]
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
@@ -10,3 +11,13 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
